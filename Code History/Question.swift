a5295018958f93b88992b0ff01e677e3007d4947
TestName <48367659+SufferedComet@users.noreply.github.com>
2023-06-19 16:49:41 -0700

    Project Init

---
+++ Code History/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Code History
+//
+//  Created by Angelo Delgado on 6/19/23.
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
