0bd45bd530cc45c7c738c87a4d1bd9625c9dc9cb
TestName <48367659+SufferedComet@users.noreply.github.com>
2023-06-21 10:29:39 -0700

    ScoreView and ScoreViewModel added

--- Code History/Views/ScoreView.swift
+++ Code History/Views/ScoreView.swift
@@ -12,6 +12,24 @@ struct ScoreView: View {
         ZStack {
             GameColor.main.ignoresSafeArea()
             VStack {
+                Spacer()
+                Text("Final Score:")
+                    .font(.body)
+                Text("???%")
+                    .font(.system(size: 50))
+                    .bold()
+                    .padding()
+                Spacer()
+                VStack {
+                    Text("??? ✅")
+                    Text("??? ❌")
+                }
+                .font(.system(size: 30))
+                Spacer()
+                NavigationLink(
+                    destination: GameView(), label: {
+                        BottomTextView(str: "Re-take Quiz")
+                    })
             }
             .foregroundColor(.white)
            // .navigationBarHidden(true) old code. Use toolbar
